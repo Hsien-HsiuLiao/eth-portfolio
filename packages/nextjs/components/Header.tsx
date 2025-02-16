9179842def87ecfe9a09fa49a9f6ffd882df32de
Hsien-Hsiu <hsien_hsiu.liao@yahoo.com>
2025-02-16 19:24:12 +0000

    reinstall npx create-eth@latest

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -9,7 +9,6 @@ import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick, useTargetNetwork } from "~~/hooks/scaffold-eth";
 
-
 type HeaderMenuLink = {
   label: string;
   href: string;
@@ -21,7 +20,7 @@ export const menuLinks: HeaderMenuLink[] = [
     label: "Home",
     href: "/",
   },
-  
+
   {
     label: "Debug Contracts",
     href: "/debug",
